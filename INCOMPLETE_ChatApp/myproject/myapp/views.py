17dddedf336a909e5e38e6c12f3b8eaaee7a456f
Mariya <mariyasha432@protonmail.com>
2022-10-20 12:25:47 -0700

    updating project files

--- INCOMPLETE_ChatApp/myproject/myapp/views.py
+++ INCOMPLETE_ChatApp/myproject/myapp/views.py
@@ -10,27 +10,23 @@ def login(request):
 	
 	user_name = request.POST["user"]
 	user_pass = request.POST["password"]
-
-	notice = ""
-	db_user = ""
 	
 	if User.objects.filter(name=user_name):
 		db_user = User.objects.get(name=user_name)
 		# user found!
-		if db_user.password == user_pass:
-			print("password matched!")
-			print("welcome back " + db_user.name + "!")
-		else:
-			print("incorrect password")	
-			notice = "oops " + db_user.name + ", this password is incorrect! please try again."	
-			return render(request, "index.html", {"notice": notice, "user": db_user })
+		if db_user.password != user_pass:
+			# incorrect password
+			notice = "oops " + db_user.name + ", this password is incorrect!"
+			action = "please try again."	
+			return render(request, "index.html", {"notice": notice, "action": action, "user": db_user })
 	else:
-		print("user not found!")
-		notice = "oops, no such user was found! please sign up below."
-		return render(request, "index.html", {"notice": notice })
+		# user not found!
+		notice = "oops, no such user was found!"
+		action = "please sign up below."
+		return render(request, "index.html", {"notice": notice, "action": action })
 
 	messages = Message.objects.all()	
-	return render(request, "chat.html", {"chat": messages, "user": db_user, "notice": notice })
+	return render(request, "chat.html", {"chat": messages, "user": db_user})
 
 def send_chat(request):
 	print("received message")
@@ -42,12 +38,9 @@ def send_chat(request):
 	
 	m = Message(message=msg, time=timezone.now(), user=db_user)
 	m.save()
-	
-	my_message = "welcome back " + db_user.name + "!"
+
 	messages = Message.objects.all()	
-	
-	print(msg)
-	return render(request, "chat.html", {"greeting": my_message, "chat": messages, "user": db_user })
+	return render(request, "chat.html", {"chat": messages, "user": db_user })
 
 def sign_up(request):
 	return render(request, "signup.html")
@@ -59,16 +52,20 @@ def create_user(request):
 	password_conf = request.POST["password_conf"]
 
 	if User.objects.filter(name=user):
-		print("oops! user exists!")
-		notice = "oops! user already exists"
-		return render(request, "signup.html", {"notice": notice})
+		# bad username
+		notice = "oops! username is already taken"
+		action = "please choose a differtent name"
+		return render(request, "signup.html", {"notice": notice, "action": action})
 	else:
 		if password == password_conf:
 			create_user = User(name=user, password=password)
 			create_user.save()
-			notice = "new user was successfully crerated! please log in."
-			return render(request, "index.html", {"notice": notice})
+			notice = "new user was successfully crerated!"
+			action = "please log in."
+			return render(request, "index.html", {"notice": notice, "action": action})
 		else:
-			notice = "oops, password fields don't match! please try again."
-			return render(request, "signup.html", {"notice": notice})
+			# bad password
+			notice = "oops, password fields don't match!"
+			action = "please try again."
+			return render(request, "signup.html", {"notice": notice, "action": action})
 	
